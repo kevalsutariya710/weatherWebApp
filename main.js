6011cdda3260ce42ec4bcd9abd2a6c6401bdf8ba
Keval Sutariya <100850842+kevalsutariya710@users.noreply.github.com>
2023-03-24 10:10:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,18 +10,11 @@ const getWeather = (city) => {
     fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
         .then(response => response.json())
         .then(response => {
-            //console.log(response)
-
-            // cloud_pct.innerHTML = response.cloud_pct
+         
             temp.innerHTML = response.temp
-            // feels_like.innerHTML = response.feels_like
             humidity.innerHTML = response.humidity
-            //min_temp.innerHTML = response.min_temp
-            // max_temp.innerHTML = response.max_temp
             wind_speed.innerHTML = response.wind_speed
-            //wind_degrees.innerHTML = response.wind_degrees
-            // sunrise.innerHTML = response.sunrise
-            // sunset.innerHTML = response.sunset
+            
         })
         .catch(err => console.error(err));
 }
